50d935eb4a813ae8cb5466721015cfac8ef16c95
S.T <tanashun@tanakashunichi-no-MacBook-Air.local>
2017-09-20 07:09:17 +0900

    repair install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # set files
 mkdir -p ~/.vim/rc/
-cp ./.vimrc ~/.vimrc
-cp ./.vim/rc/dein.toml ~/.vim/rc/dein.toml
-cp ./.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
-cp ./.tmux.conf ~/.tmux.conf
+cp ./vim/.vimrc ~/.vimrc
+cp ./vim/.vim/rc/dein.toml ~/.vim/rc/dein.toml
+cp ./vim/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
+cp ./tmux/.tmux.conf ~/.tmux.conf
