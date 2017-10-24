a2c542ab2eb03d19e0a1948af0f9f1ad69bcdf4b
tanaka-shu <tanaka-shu@gnavi.co.jp>
2017-10-24 00:56:00 +0000

    repair

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 # set files
-mkdir -p ~/.vim/rc/
-cp ./vim/.vimrc ~/.vimrc
-cp ./vim/.vim/rc/dein.toml ~/.vim/rc/dein.toml
-cp ./vim/.vim/rc/dein_lazy.toml ~/.vim/rc/dein_lazy.toml
+mkdir -p ~/.vim/plugin
+mkdir -p ~/.vim/dict
+cp ./vim/.vimrc.noplugin ~/.vimrc
 cp ./tmux/.tmux.conf ~/.tmux.conf
+
+# vim plugin
+cd ~/.vim/plugin/
+git clone https://github.com/vim-scripts/AutoComplPop.git
+
+# php dictionary
+php -r '$f=get_defined_functions();echo join("\n",$f["internal"]);'|sort > ~/.vim/dict/php.dict
+
