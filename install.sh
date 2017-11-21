884a05202e21d1cc5481a80e58c153f5d692e2ec
ShunichiTanaka <nekodanshaku27@gmail.com>
2017-11-21 18:15:04 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,13 @@ cp ./tmux/.tmux.conf ~/.tmux.conf
 
 # vim plugin
 cd ~/.vim/plugin/
-git clone https://github.com/vim-scripts/AutoComplPop.git
+if [! -e ~/.vim/plugin/AutoComplPop]; then
+    git clone https://github.com/vim-scripts/AutoComplPop.git
+fi
 cd ~/.vim/colors/
-git clone https://github.com/tomasr/molokai.git
+if [! -e ~/.vim/plugin/molokai]; then
+    git clone https://github.com/tomasr/molokai.git
+fi
 
 # php dictionary
 php -r '$f=get_defined_functions();echo join("\n",$f["internal"]);'|sort > ~/.vim/dict/php.dict
