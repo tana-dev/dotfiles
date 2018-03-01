c3a31315527c6a6f67ecb60a569021eb8be98c0e
Shunichi Tanaka <nekodanshaku27@gmail.com>
2018-03-01 09:15:05 +0900

    vimrc修正

--- install.sh
+++ install.sh
@@ -8,17 +8,11 @@ cp ./git/.gitconfig ~/.gitconfig
 mkdir -p ~/.vim/plugin/
 mkdir -p ~/.vim/dict/
 mkdir -p ~/.vim/colors/
-mkdir -p ~/vim_colors/
 
 # vim plugin
 cd ~/.vim/plugin/
 if [ ! -e ~/.vim/plugin/AutoComplPop ]; then
 	git clone https://github.com/vim-scripts/AutoComplPop.git
-fi
-cd ~/vim_colors/
-if [ ! -e ~/.vim/colors/molokai.vim ]; then
-	git clone https://github.com/tomasr/molokai.git
-	mv molokai/colors/molokai.vim ~/.vim/colors/
 fi
 
 # php dictionary
