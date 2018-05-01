0aed4821664be0afc0acfda6329f318c81d9562c
Shunichi Tanaka <nekodanshaku27@gmail.com>
2018-05-01 10:25:46 +0900

    repair

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # set files
 cp ./bash/.bash_profile ~/.bash_profile
+cp ./bash/.bashrc ~/.bashrc
 cp ./vim/.vimrc.noplugin ~/.vimrc
 cp ./tmux/.tmux.conf ~/.tmux.conf
 cp ./git/.gitconfig ~/.gitconfig
@@ -17,3 +18,4 @@ fi
 
 # php dictionary
 php -r '$f=get_defined_functions();echo join("\n",$f["internal"]);'|sort > ~/.vim/dict/php.dict
+
